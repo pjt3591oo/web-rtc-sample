2d058f5676430f1fe731eeeedd8da6f1058c6968
pjt3591oo <pjt3591oo@gmail.com>
2021-11-01 10:55:13 +0900

    화면공유?

--- README.md
+++ README.md
@@ -1,9 +1,1 @@
 # webrtc
-
-```
-MediaStream: 카메라, 마이크 접근
-
-RTCPeerConnection: 
-
-RTCDataChannel
-```
--- public/index.js
+++ public/index.js
@@ -5,26 +5,31 @@ const myPeer = new Peer();
 const myVideo = document.createElement('video');
 myVideo.muted = true;
 const peers = {};
+let calls;
+let uid;
 
 navigator.mediaDevices
   .getUserMedia({
     video: true,
-    audio: false,
+    audio: true,
   }).then((stream) => {
     addVideoStream(myVideo, stream);
     
     // 다른 유저가 스트림 요청
     // 다른 유저가 스트림 요청할 때 본인의 스트림을 보내줌
     myPeer.on('call', (call) => { 
+      
       const video = document.createElement('video');
 
       // 유저가 스트림 요청할 때 본인의 스틀림을 받아서 화면에 출력
       call.on('stream', (stream) => {
+        console.log('stream');
         addVideoStream(video, stream);
       });
 
       call.answer(stream);  // 스트림 전달
     });
+
 
     socket.on('user-connected', (userId) => {
       connectToNewUser(userId, stream);
@@ -37,6 +42,7 @@ socket.on('user-disconnected', (userId) => {
 
 myPeer.on('open', (userId) => {
   const roomId = 10;
+  uid = userId;
   console.log(`[ON] open: ${userId}`);
   
   socket.emit('join-room', roomId, userId);
@@ -48,19 +54,45 @@ function connectToNewUser(userId, stream) {
   // 접속한 유저에게 스트림 요청
   // 요청할 때 본인의 스트림을 전달한다.
   const call = myPeer.call(userId, stream); 
-  
+  // console.log(call.peerConnection)
+  calls = call.peerConnection;
   const video = document.createElement('video');
   
   // call 대상이 answer()로 응답한 스트림을 전달받아서 화면에 출력
   call.on('stream', (userVideoStream) => { // userId에게 stream을 받음
-    console.log(2);
     addVideoStream(video, userVideoStream); // 유저에게 받은 stream을 화면에 출력
   });
 
   call.on('close', () => {
     video.remove();
   });
+
   peers[userId] = call;
+}
+
+myPeer.on('connection', function(conn) {
+  console.log('connection');
+  // conn.on('data', function(data){
+  //   // Will print 'hi!'
+  //   console.log(data);
+  // });
+});
+
+// 화면공유
+async function shareScreen(e) {
+  const screenStream = await navigator.mediaDevices.getDisplayMedia({
+    video: true,
+    audio: false
+  })
+  console.log('screenStream')
+  console.log(screenStream)
+  const sender = calls.getSenders().find(s => {
+    console.log(s.track.kind, screenStream);
+    return s.track.kind === 'video'
+  });
+  console.log(sender);
+  sender.replaceTrack(screenStream.getVideoTracks()[0]);
+  addVideoStream(myVideo, screenStream)
 }
 
 function addVideoStream(video, stream) {
@@ -70,3 +102,5 @@ function addVideoStream(video, stream) {
   });
   videoGrid.append(video);
 }
+
+document.getElementById('screen-share').addEventListener('click', shareScreen);
