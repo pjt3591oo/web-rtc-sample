8ef7ba4aece64f2e095f5c6696f4ab59cc2e5416
pjt3591oo <pjt3591oo@gmail.com>
2021-11-01 22:52:26 +0900

    improve.html 추가, 좀 더 정교한 스트림 제어

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # webrtc
+
+* peer server
+
+```
+$ peerjs --port 9000 --key peerjs --path /myapp 
+```
--- public/index.js
+++ public/index.js
@@ -5,16 +5,18 @@ const myPeer = new Peer();
 const myVideo = document.createElement('video');
 myVideo.muted = true;
 const peers = {};
+let myStream;
 let calls;
 let uid;
 
 navigator.mediaDevices
   .getUserMedia({
     video: true,
-    audio: true,
+    audio: false,
   }).then((stream) => {
+    console.log('my stream id: ', stream.id);
     addVideoStream(myVideo, stream);
-    
+    myStream = stream;
     // 다른 유저가 스트림 요청
     // 다른 유저가 스트림 요청할 때 본인의 스트림을 보내줌
     myPeer.on('call', (call) => { 
@@ -23,7 +25,7 @@ navigator.mediaDevices
 
       // 유저가 스트림 요청할 때 본인의 스틀림을 받아서 화면에 출력
       call.on('stream', (stream) => {
-        console.log('stream');
+        console.log('stream', stream);
         addVideoStream(video, stream);
       });
 
@@ -54,8 +56,6 @@ function connectToNewUser(userId, stream) {
   // 접속한 유저에게 스트림 요청
   // 요청할 때 본인의 스트림을 전달한다.
   const call = myPeer.call(userId, stream); 
-  // console.log(call.peerConnection)
-  calls = call.peerConnection;
   const video = document.createElement('video');
   
   // call 대상이 answer()로 응답한 스트림을 전달받아서 화면에 출력
@@ -84,15 +84,19 @@ async function shareScreen(e) {
     video: true,
     audio: false
   })
-  console.log('screenStream')
-  console.log(screenStream)
-  const sender = calls.getSenders().find(s => {
-    console.log(s.track.kind, screenStream);
-    return s.track.kind === 'video'
-  });
-  console.log(sender);
-  sender.replaceTrack(screenStream.getVideoTracks()[0]);
-  addVideoStream(myVideo, screenStream)
+  console.log('screenStream', uid)
+  // myPeer.call(uid, screenStream); 
+  Object.keys(peers).forEach(peer => {
+    peers[peer].call(uid, screenStream)
+  })
+  // const sender = calls.getSenders().find(s => {
+  //   console.log(s.track.kind, screenStream);
+  //   return s.track.kind === 'video'
+  // });
+  
+  
+  // sender.replaceTrack(screenStream.getVideoTracks()[0]);
+  // addVideoStream(myVideo, screenStream)
 }
 
 function addVideoStream(video, stream) {
@@ -100,6 +104,7 @@ function addVideoStream(video, stream) {
   video.addEventListener('loadedmetadata', () => {
     video.play();
   });
+  video.setAttribute('data-peer-id', stream.id);
   videoGrid.append(video);
 }
 
